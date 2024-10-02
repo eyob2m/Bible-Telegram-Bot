804cef3fca91be6396cfcd768e7c234e9ce0498b
eyob2m <eyob2etemariam@gmail.com>
2024-10-02 04:10:54 +0300

    moved

--- webhook.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-const setWebhook = async () => {
-    const response = await axios.get(`https://api.telegram.org/bot5758248073:AAHhRY67sCd7wDIbwTtJo6YMRNQFQfiE9dU/setWebhook?url=https://bible-bot.vercel.app/`);
-    console.log(response.data);
-};
-
-setWebhook();
